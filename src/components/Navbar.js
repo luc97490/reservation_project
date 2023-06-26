d345e28453e7fa816385f65b17ce6084c0ed72f3
Pascal Randria <974pascalito974@gmail.com>
2023-06-26 16:27:34 +0400

    Move(Sidebar) ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,103 +1,32 @@
-export default function Navbar() {
+"use client";
+import { Dropdown } from "flowbite-react";
+export default function Navbar({ isButtonClicked }) {
+  const mlNav = isButtonClicked ? "ml-24" : "ml-72";
   return (
-    <nav className="fixed top-0 right-0 z-50 w-1246 rounded-t-xl h-20 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
+    <nav
+      className={`fixed top-0 left-0 z-40  ${mlNav} w-[fill-available] rounded-t-xl h-20 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700`}
+    >
+      <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+        <div></div>
         <div className="flex items-center md:order-2">
-          <button
-            type="button"
-            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-            id="user-menu-button"
-            aria-expanded="false"
-            data-dropdown-toggle="user-dropdown"
-            data-dropdown-placement="bottom"
+          <label
+            tabIndex={0}
+            className="btn mx-12 rounded-full drop-shadow-2xl border-none bg-primary-light dark:bg-primary-dark btn-circle w-14 h-14 "
           >
-            <span className="sr-only">Open user menu</span>
-            <img
-              className="w-8 h-8 rounded-full"
-              src="/docs/images/people/profile-picture-3.jpg"
-              alt="user photo"
-            />
-          </button>
-          {/* Dropdown menu */}
-          <div
-            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-            id="user-dropdown"
-          >
-            <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 dark:text-white">
-                Bonnie Green
-              </span>
-              <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                name@flowbite.com
-              </span>
-            </div>
-            <ul className="py-2" aria-labelledby="user-menu-button">
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Earnings
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Sign out
-                </a>
-              </li>
-            </ul>
-          </div>
-          <button
-            data-collapse-toggle="mobile-menu-2"
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu-2"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+            <Dropdown>
+              <Dropdown.Header>
+                <span className="block text-sm">Bonnie Green</span>
+                <span className="block truncate text-sm font-medium">
+                  bonnie@flowbite.com
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Item>Dashboard</Dropdown.Item>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Item>Earnings</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+          </label>
         </div>
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
@@ -110,7 +39,7 @@ export default function Navbar() {
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
-                Home
+                Accueil
               </a>
             </li>
             <li>
@@ -118,7 +47,7 @@ export default function Navbar() {
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                About
+                Matériels
               </a>
             </li>
             <li>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@
 
 import { DarkThemeToggle, Flowbite, Sidebar } from "flowbite-react";
 import Link from "next/link";
+import { useState } from "react";
 import {
   HiArrowSmRight,
   HiChartPie,
@@ -13,20 +14,122 @@ import {
   BiBuoy,
 } from "react-icons/hi";
 
-export default function SideBar() {
+export default function SideBar({ isButtonClicked }) {
+  const wSide = isButtonClicked ? "w-24" : "w-72";
+  const centerIcon = isButtonClicked ? "justify-center" : "mx-7";
+  const hiddenText = isButtonClicked ? "hidden" : "";
+
   return (
     <>
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-50 w-72 h-screen rounded-2xl border transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        className={`fixed top-0  left-0 z-40 ${wSide} transition-all h-screen rounded-2xl border bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`}
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 pb-4 overflow-y-auto ">
+        <div className={`h-full px-3 pb-4 overflow-y-auto `}>
           <ul className="space-y-2 font-medium">
+            <li>
+              <Link href="/" className="w-full">
+                <div className="logo h-20 w-full border-b border-border-light dark:border-border-dark rounded-t-2xl h-19 bg-secondary-light dark:bg-secondary-dark flex gap-5 flex-row flex-wrap items-center justify-center">
+                  <svg
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 40 40"
+                    width="40"
+                    height="40"
+                  >
+                    <g clip-path="url(#a)">
+                      <path
+                        d="m19 0-.6.2c-.9 0-1.9.4-2.7.9a9.5 9.5 0 0 0-4.8 6c-.2.7-.2.9-.2 2.3 0 1.5 0 1.6.2 2.3.4 1.7 1.2 3.1 2.5 4.3a9.3 9.3 0 0 0 8.9 2.5c3.4-.9 6-3.4 6.8-6.8.2-.7.2-.9.2-2.3a7 7 0 0 0-.2-2.3 9.5 9.5 0 0 0-10-7Zm5.4 6.3.8.8-3.2 3.1-3.2 3.2-1.8-1.8-1.8-1.8.9-.8.8-.8 1 1 .9.9L21 7.8l2.4-2.3.9.8Z"
+                        fill="url(#b)"
+                      />
+                      <path
+                        d="M2.3 22.4V33h35.4V11.8h-6.2v.3a11.9 11.9 0 0 1-7.8 8.5 11.8 11.8 0 0 1-15.1-8.4l-.1-.4H2.3v10.6ZM13 24.7v1.2H7v-2.4h6v1.2ZM33 27v3.6H23.5v-7.1H33V27Zm-13 2.4v1.2H7.1v-2.3l6.5-.1H20v1.2Z"
+                        fill="url(#c)"
+                      />
+                      <path
+                        d="m26 26-.1 1v1.2H30.6v-2.3H25.9Z"
+                        fill="url(#d)"
+                      />
+                      <path
+                        d="M.2 35.4.8 37l1 1.3c.6.6.8.7 1.4 1 1.4.7-.2.7 16.8.7s15.4 0 16.8-.7c.6-.3.8-.4 1.4-1l1-1.3.6-1.6H.2Z"
+                        fill="url(#e)"
+                      />
+                    </g>
+                    <defs>
+                      <linearGradient
+                        id="b"
+                        x1="9.3"
+                        y1="4.1"
+                        x2="36"
+                        y2="22.6"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" />
+                        <stop
+                          offset="1"
+                          stop-color="#8B8B8B"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                      <linearGradient
+                        id="c"
+                        x1="-.3"
+                        y1="16.5"
+                        x2="31.6"
+                        y2="53.4"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" />
+                        <stop
+                          offset="1"
+                          stop-color="#8B8B8B"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                      <linearGradient
+                        id="d"
+                        x1="25.5"
+                        y1="26.4"
+                        x2="28.8"
+                        y2="31.1"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" />
+                        <stop
+                          offset="1"
+                          stop-color="#8B8B8B"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                      <linearGradient
+                        id="e"
+                        x1="-2.8"
+                        y1="36.3"
+                        x2="-.5"
+                        y2="50.1"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" />
+                        <stop
+                          offset="1"
+                          stop-color="#8B8B8B"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                      <clipPath id="a">
+                        <path fill="#fff" d="M0 0H40V40H0z" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  <span className={hiddenText}>MIO - RESERVATION</span>
+                </div>
+              </Link>
+            </li>
             <li>
               <Link
                 href="/demande"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -38,13 +141,15 @@ export default function SideBar() {
                   <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
                   <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
                 </svg>
-                <span className="ml-3">Dashboard</span>
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Dashboard
+                </span>
               </Link>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -55,16 +160,15 @@ export default function SideBar() {
                 >
                   <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  Pro
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Kanban
                 </span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -76,16 +180,15 @@ export default function SideBar() {
                   <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" />
                   <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  3
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Inbox
                 </span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -100,13 +203,15 @@ export default function SideBar() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Users
+                </span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -121,13 +226,15 @@ export default function SideBar() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Products
+                </span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -142,13 +249,15 @@ export default function SideBar() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Sign In
+                </span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${centerIcon}`}
               >
                 <svg
                   aria-hidden="true"
@@ -163,7 +272,9 @@ export default function SideBar() {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                <span className={`flex-1 whitespace-nowrap ml-3 ${hiddenText}`}>
+                  Sign Up
+                </span>
               </a>
             </li>
             <li>
