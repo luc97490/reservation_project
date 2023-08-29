39b4ff6310a74f97d82dc72992c158372c4a9494
luc97490 <randria.pascal@hotmail.com>
2023-08-29 13:17:35 +0400

    ...

--- src/components/form/ReservationUser.js
+++ src/components/form/ReservationUser.js
@@ -191,7 +191,7 @@ export default function ReservationUser() {
               type="datetime-local"
               id="debut"
               name="debut"
-              className="bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
             />
           </div>
